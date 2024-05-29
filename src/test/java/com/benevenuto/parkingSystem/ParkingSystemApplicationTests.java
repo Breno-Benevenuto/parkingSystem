721a92f3eb459266f7fa746873e94707b7d4b07c
Breno Benevenuto <brenobenevenuto@gmail.com>
2024-05-28 22:54:34 -0300

    Create Project

---
+++ src/test/java/com/benevenuto/parkingSystem/ParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benevenuto.parkingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
